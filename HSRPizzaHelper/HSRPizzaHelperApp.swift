8c1a94f9ffbc10b5104cb9c00590bde8bf0dbfa5
Bill-Haku <battleandwar@hotmail.com>
2023-04-26 13:45:00 +0800

    Initial Commit

---
+++ HSRPizzaHelper/HSRPizzaHelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSRPizzaHelperApp.swift
+//  HSRPizzaHelper
+//
+//  Created by Bill Haku on 2023/4/26.
+//
+
+import SwiftUI
+
+@main
+struct HSRPizzaHelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
