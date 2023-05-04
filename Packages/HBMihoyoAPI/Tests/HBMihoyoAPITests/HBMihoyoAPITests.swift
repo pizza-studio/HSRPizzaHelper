e6d971c50206d8efc2d60e0f874ee90617fa7c24
Lava <34743145+CanglongCl@users.noreply.github.com>
2023-05-04 14:14:32 +0800

    code: DailyNote test

--- Packages/HBMihoyoAPI/Tests/HBMihoyoAPITests/HBMihoyoAPITests.swift
+++ Packages/HBMihoyoAPI/Tests/HBMihoyoAPITests/HBMihoyoAPITests.swift
@@ -7,4 +7,10 @@ final class HBMihoyoAPITests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
     }
+
+    func dailyNoteDecodeTest() throws {
+        let exampleURL = Bundle.module.url(forResource: "daily_note_example", withExtension: "json")!
+        let exampleData = try Data(contentsOf: exampleURL)
+        _ = try DailyNote.decodeFromMiHoYoAPIJSONResult(data: exampleData)
+    }
 }
