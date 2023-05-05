7f4d0e48d840647676a8abebe97ff440c2324213
Lava <34743145+CanglongCl@users.noreply.github.com>
2023-05-05 12:58:34 +0800

    Manage Account // fix add account (#10)
    * fix: fix add account
    
    * fix: select account when creating account
    
    * i18n: apply i18n

---
+++ Features/Manage Accounts/View/EditAccountSheetView.swift
@@ -0,0 +1,95 @@
+//
+//  EditAccountSheetView.swift
+//  HSRPizzaHelper
+//
+//  Created by 戴藏龙 on 2023/5/5.
+//
+
+import SwiftUI
+
+// MARK: - EditAccountSheetView
+
+struct EditAccountSheetView: View {
+    // MARK: Lifecycle
+
+    init(account: Account, isShown: Binding<Bool>) {
+        self._account = StateObject(wrappedValue: account)
+        self._isShown = isShown
+    }
+
+    // MARK: Internal
+
+    var body: some View {
+        NavigationView {
+            List {
+                EditAccountView(account: account)
+            }
+            .navigationTitle("Edit Account")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Done") {
+                        viewContext.performAndWait {
+                            do {
+                                try viewContext.save()
+                                isShown.toggle()
+                            } catch {
+                                saveAccountError = .saveDataError(error)
+                                isSaveAccountFailAlertShown.toggle()
+                            }
+                        }
+                    }
+                }
+            }
+            .alert(isPresented: $isSaveAccountFailAlertShown, error: saveAccountError) {
+                Button("OK") {
+                    isSaveAccountFailAlertShown.toggle()
+                }
+            }
+        }
+    }
+
+    // MARK: Private
+
+    @Environment(\.managedObjectContext) private var viewContext
+
+    @StateObject private var account: Account
+
+    @Binding private var isShown: Bool
+
+    @State private var isSaveAccountFailAlertShown: Bool = false
+    @State private var saveAccountError: SaveAccountError?
+}
+
+// MARK: - SaveAccountError
+
+private enum SaveAccountError {
+    case saveDataError(Error)
+    case missingFieldError(String)
+}
+
+// MARK: LocalizedError
+
+extension SaveAccountError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case let .saveDataError(error):
+            return "Save Account Fail\nSave Error: \(error).\nPlease try again."
+        case let .missingFieldError(field):
+            return "Save Account Fail\nMissing Fields: \(field).\nPlease try again."
+        }
+    }
+
+    var failureReason: String? {
+        switch self {
+        case let .saveDataError(error):
+            return "Save Error: \(error)."
+        case let .missingFieldError(field):
+            return "Missing Fields: \(field)."
+        }
+    }
+
+    var helpAnchor: String? {
+        "Please try login again. "
+    }
+}
