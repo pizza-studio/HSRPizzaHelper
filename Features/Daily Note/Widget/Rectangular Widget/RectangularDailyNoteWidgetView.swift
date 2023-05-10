1ce30f99b9cbe0041229f86a848cb7c198f01822
Bill-Haku <battleandwar@hotmail.com>
2023-05-10 11:33:58 +0800

    feat: widget ui

--- Features/Daily Note/Widget/Rectangular Widget/RectangularDailyNoteWidgetView.swift
+++ Features/Daily Note/Widget/Rectangular Widget/RectangularDailyNoteWidgetView.swift
@@ -24,7 +24,7 @@ struct RectangularDailyNoteWidgetView: View {
                 Text(error.localizedDescription)
             }
         }
-        .padding(mainViewPadding)
+        .padding(.vertical, mainViewPadding)
         .background {
             VStack {
                 HStack {
