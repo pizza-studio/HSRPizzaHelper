2572b267daebdb5999436f36ae92bf805ad1d991
Lava <34743145+CanglongCl@users.noreply.github.com>
2023-05-14 11:10:58 +0800

    fix: customize stamina notification

--- Common/Notification/DailyNoteNotification.swift
+++ Common/Notification/DailyNoteNotification.swift
@@ -160,7 +160,7 @@ private struct DailyNoteNotificationSender {
         )
         content.badge = 1
 
-        let timeInterval = information.remainingTime - Double(staminaNumber) * StaminaInformation
+        let timeInterval = information.remainingTime - Double(information.maxStamina - staminaNumber) * StaminaInformation
             .eachStaminaRecoveryTime
         guard timeInterval > 0 else { return }
 
