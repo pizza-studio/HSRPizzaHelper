317ee1a12adad21eb64530c4d69bb2c6d7a74a30
Lava <34743145+CanglongCl@users.noreply.github.com>
2023-07-08 16:27:01 +0800

    fix: github action build fail because of package deployment of watch os version

--- Packages/HBMihoyoAPI/Package.swift
+++ Packages/HBMihoyoAPI/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "HBMihoyoAPI",
     platforms: [
         .iOS(.v15),
+        .watchOS(.v9)
     ],
     products: [
         .library(
--- Packages/HBPizzaHelperAPI/Package.swift
+++ Packages/HBPizzaHelperAPI/Package.swift
@@ -7,6 +7,7 @@ let package = Package(
     name: "HBPizzaHelperAPI",
     platforms: [
         .iOS(.v15),
+        .watchOS(.v9)
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
