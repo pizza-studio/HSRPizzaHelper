8928df300bff61f133c8b03c5acd8dc5022310ee
Lava <34743145+CanglongCl@users.noreply.github.com>
2023-07-30 18:00:31 +0800

    Create ToolView.swift

---
+++ HSRPizzaHelper/View/ToolView.swift
@@ -0,0 +1,22 @@
+//
+//  ToolView.swift
+//  HSRPizzaHelper
+//
+//  Created by 戴藏龙 on 2023/7/30.
+//
+
+import Foundation
+import SwiftUI
+
+struct ToolView: View {
+    var body: some View {
+        NavigationView {
+            List {
+                NavigationLink("app.tool.dictionary") {
+                    HSRDictionaryView()
+                }
+            }
+            .navigationTitle("app.tool.title")
+        }
+    }
+}
