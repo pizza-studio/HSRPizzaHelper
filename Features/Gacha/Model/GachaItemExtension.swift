74a555008bf87a18dbd7161ae29848a337f6e0af
Lava <34743145+CanglongCl@users.noreply.github.com>
2023-08-11 11:24:55 +0800

    add localizedName and icon for gacha item

---
+++ Features/Gacha/Model/GachaItemExtension.swift
@@ -0,0 +1,20 @@
+//
+//  GachaItemExtension.swift
+//  HSRPizzaHelper
+//
+//  Created by 戴藏龙 on 2023/8/11.
+//
+
+import Foundation
+import HBMihoyoAPI
+import UIKit
+
+extension GachaItem {
+    var localizedName: String {
+        GachaMetaManager.shared.getLocalizedName(id: itemID, type: itemType) ?? name
+    }
+
+    var icon: UIImage? {
+        GachaMetaManager.shared.getIcon(id: itemID, type: itemType)
+    }
+}
