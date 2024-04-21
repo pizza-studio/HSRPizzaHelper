8e53846a8326bce5fa97a6729b11358d02cf5844
ShikiSuen <shikisuen@outlook.com>
2024-04-21 12:52:49 +0800

    Revert "DPV // Only refresh on user-intent."
    This reverts commit 0d72ab52c8f49419da41b16aec51a1ac8a8b802b.

--- HSRPizzaHelper/View/DetailPortalView.swift
+++ HSRPizzaHelper/View/DetailPortalView.swift
@@ -3,7 +3,6 @@
 // This code is released under the GPL v3.0 License (SPDX-License-Identifier: GPL-3.0)
 
 import Combine
-import Defaults
 import EnkaSwiftUIViews
 import Foundation
 import HBEnkaAPI
@@ -41,15 +40,7 @@ final class DetailPortalViewModel: ObservableObject {
     let enkaDB = EnkaHSR.EnkaDB(locTag: Locale.langCodeForEnkaAPI)!
 
     @Published var selectedAccount: Account? {
-        didSet {
-            if let newAccountUID = selectedAccount?.uid {
-                currentBasicInfo = Defaults[.queriedEnkaProfiles][newAccountUID]
-                if currentBasicInfo == nil {
-                    fetchPlayerDetail()
-                }
-                detailPortalRefreshSubject.send(())
-            }
-        }
+        didSet { refresh() }
     }
 
     // swiftlint:enable force_unwrapping
