a5e52265a6144378a93fcc5a25c9e331661b12b0
ShikiSuen <shikisuen@outlook.com>
2024-04-26 19:27:41 +0800

    SRGFKit // File header fix.

--- Packages/SRGFKit/Sources/SRGFKit/SRGFv1.swift
+++ Packages/SRGFKit/Sources/SRGFKit/SRGFv1.swift
@@ -1,4 +1,6 @@
-// Ref: https://uigf.org/zh/standards/srgf.html
+// (c) 2023 and onwards Pizza Studio (GPL v3.0 License).
+// ====================
+// This code is released under the GPL v3.0 License (SPDX-License-Identifier: GPL-3.0)
 
 // MARK: - SRGFv1
 
