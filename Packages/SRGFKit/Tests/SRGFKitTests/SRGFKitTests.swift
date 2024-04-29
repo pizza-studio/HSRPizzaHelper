338b075efced36171cd260977dac93604e011f8a
ShikiSuen <shikisuen@outlook.com>
2024-04-29 10:56:07 +0800

    SRGFKit // Add consistency tests against before-after conversion.

--- Packages/SRGFKit/Tests/SRGFKitTests/SRGFKitTests.swift
+++ Packages/SRGFKit/Tests/SRGFKitTests/SRGFKitTests.swift
@@ -35,4 +35,16 @@ final class SRGFKitTests: XCTestCase {
         XCTAssertEqual(y.count, 1)
         print(y.compactMap(\.name).joined(separator: "\n"))
     }
+
+    func testConsistency() throws {
+        let filePath: String = testDataPath + "SRGFv1_Sample.json"
+        let url = URL(fileURLWithPath: filePath)
+        let data = try Data(contentsOf: url)
+        let decoded = try JSONDecoder().decode(SRGFv1.self, from: data)
+        let gachaLang = decoded.info.lang
+        let rawList = decoded.list.sorted { $0.id < $1.id }
+        let gachaEntries = rawList.map { $0.toGachaEntry(uid: "114514810", lang: gachaLang) }
+        let newList = gachaEntries.map { $0.toSRGFEntry(langOverride: gachaLang) }.sorted { $0.id < $1.id }
+        XCTAssertEqual(rawList, newList)
+    }
 }
