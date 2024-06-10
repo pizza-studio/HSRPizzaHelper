0bb4859c53f63cab160e71d435f54f5da9c0ee0f
ShikiSuen <shikisuen@outlook.com>
2024-06-10 14:25:05 +0800

    HBMihoyoAPI // Refactor GetDeviceFingerprint().

--- Packages/HBMihoyoAPI/Sources/HBMihoyoAPI/GenerateDeviceFingerPrintAPI/GetDeviceFingerPrint.swift
+++ Packages/HBMihoyoAPI/Sources/HBMihoyoAPI/GenerateDeviceFingerPrintAPI/GetDeviceFingerPrint.swift
@@ -17,16 +17,6 @@ extension MiHoYoAPI {
         if let fingerPrint = Defaults[.deviceFingerPrint], !fingerPrint.isEmpty {
             return fingerPrint
         }
-        func generateSeed() -> String {
-            let characters = "0123456789abcdef"
-            var result = ""
-            for _ in 0 ..< 16 {
-                let randomIndex = Int.random(in: 0 ..< characters.count)
-                let character = characters[characters.index(characters.startIndex, offsetBy: randomIndex)]
-                result.append(character)
-            }
-            return result
-        }
 
         struct DeviceFingerPrintResult: DecodableFromMiHoYoAPIJSONResult {
             let msg: String
@@ -35,24 +25,21 @@ extension MiHoYoAPI {
             let code: Int
         }
 
-        let url = URL(string: "https://public-data-api.mihoyo.com/device-fp/api/getFp")!
+        let url = URL(string: "\(region.publicDataHostURLHeader)/device-fp/api/getFp")!
         #if os(iOS)
         let deviceId = await (UIDevice.current.identifierForVendor ?? UUID()).uuidString
         #else
         let deviceId = UUID().uuidString
         #endif
+        let platformID: Int = region == .mainlandChina ? 1 : 5
         let body: [String: String] = [
             "seed_id": generateSeed(),
             "device_id": deviceId,
-            "platform": "5",
+            "platform": "\(platformID)",
             "seed_time": "\(Int(Date().timeIntervalSince1970) * 1000)",
-            // swiftlint:disable line_length
-            "ext_fields": """
-            {"userAgent":"Mozilla/5.0 (iPhone; CPU iPhone OS 16_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) miHoYoBBS/2.50.1","browserScreenSize":281520,"maxTouchPoints":5,"isTouchSupported":true,"browserLanguage":"zh-CN","browserPlat":"iPhone","browserTimeZone":"Asia/Shanghai","webGlRender":"Apple GPU","webGlVendor":"Apple Inc.","numOfPlugins":0,"listOfPlugins":"unknown","screenRatio":3,"deviceMemory":"unknown","hardwareConcurrency":"4","cpuClass":"unknown","ifNotTrack":"unknown","ifAdBlock":0,"hasLiedResolution":1,"hasLiedOs":0,"hasLiedBrowser":0}
-            """,
-            // swiftlint:enable line_length
-            "app_name": "account_cn",
-            "device_fp": "38d7ee834d1e9",
+            "ext_fields": region.getFpExtFields(deviceID: deviceId),
+            "app_name": region.appNameStringForFp,
+            "device_fp": region.deviceFpString,
         ]
         var request = URLRequest(url: url)
         request.httpBody = try JSONEncoder().encode(body)
@@ -61,5 +48,62 @@ extension MiHoYoAPI {
         let fingerPrint = try DeviceFingerPrintResult.decodeFromMiHoYoAPIJSONResult(data: data)
             .device_fp
         return fingerPrint
+    }
+}
+
+private func generateSeed(_ length: Int = 16) -> String {
+    let length = Swift.max(0, length) // 防呆
+    let characters = "0123456789abcdef".map(\.description)
+    var result = ""
+    while result.count < length, let randE = characters.randomElement() {
+        result.append(randE)
+    }
+    return result
+}
+
+extension Region {
+    fileprivate var publicDataHostURLHeader: String {
+        switch self {
+        case .mainlandChina: return "https://public-data-api.mihoyo.com"
+        case .global: return "https://sg-public-data-api.hoyoverse.com"
+        }
+    }
+
+    fileprivate var appNameStringForFp: String {
+        switch self {
+        case .mainlandChina: return "account_cn"
+        case .global: return "hkrpg_global"
+        }
+    }
+
+    fileprivate var deviceFpString: String {
+        switch self {
+        case .mainlandChina: return "38d7f0fa36179"
+        case .global: return "38d7f2364db95"
+        }
+    }
+
+    fileprivate func getFpExtFields(deviceID: String) -> String {
+        switch self {
+        case .mainlandChina:
+            // swiftlint:disable line_length
+            return """
+            {"ramCapacity":"3746","hasVpn":"0","proxyStatus":"0","screenBrightness":"0.550","packageName":"com.miHoYo.mhybbs","romRemain":"100513","deviceName":"iPhone","isJailBreak":"0","magnetometer":"-160.495300x-206.488358x58.534348","buildTime":"1706406805675","ramRemain":"97","accelerometer":"-0.419876x-0.748367x-0.508057","cpuCores":"6","cpuType":"CPU_TYPE_ARM64","packageVersion":"2.20.1","gyroscope":"0.133974x-0.051780x-0.062961","batteryStatus":"45","appUpdateTimeDiff":"1707130080397","appMemory":"57","screenSize":"414×896","vendor":"--","model":"iPhone12,5","IDFV":"\(
+                deviceID
+                    .uppercased()
+            )","romCapacity":"488153","isPushEnabled":"1","appInstallTimeDiff":"1696756955347","osVersion":"17.2.1","chargeStatus":"1","isSimInserted":"1","networkType":"WIFI"}
+            """
+        // swiftlint:enable line_length
+        case .global:
+            // swiftlint:disable line_length
+            return """
+            {"userAgent":"Mozilla/5.0 (Linux; Android 11; J9110 Build/55.2.A.4.332; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/124.0.6367.179 Mobile Safari/537.36 miHoYoBBSOversea/2.55.0","browserScreenSize":"387904","maxTouchPoints":"5","isTouchSupported":"1","browserLanguage":"zh-CN","browserPlat":"Linux aarch64","browserTimeZone":"Asia/Shanghai","webGlRender":"Adreno (TM) 640","webGlVendor":"Qualcomm","numOfPlugins":"0","listOfPlugins":"unknown","screenRatio":"2.625","deviceMemory":"4","hardwareConcurrency":"8","cpuClass":"unknown","ifNotTrack":"unknown","ifAdBlock":"0","hasLiedLanguage":"0","hasLiedResolution":"1","hasLiedOs":"0","hasLiedBrowser":"0","canvas":"\(
+                generateSeed(64)
+            )","webDriver":"0","colorDepth":"24","pixelRatio":"2.625","packageName":"unknown","packageVersion":"2.27.0","webgl":"\(
+                generateSeed(64)
+            )"}
+            """
+            // swiftlint:enable line_length
+        }
     }
 }
