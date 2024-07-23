f187ec0fb2b4fd8f18497b0a75a893964b78c70e
ShikiSuen <shikisuen@outlook.com>
2024-07-23 12:34:50 +0800

    Revert "Scripts // Add a new GachaMetaDB compiler for future purposes."
    This reverts commit 3d4f3d63dcdc8ec73a4ca016ceba22d6081ba2cf.
    
    [REASON] Contents migrated to a dedicated repo: https://github.com/pizza-studio/GachaMetaGenerator

--- Script/ArchivedForFuturePurposes/GachaMetaDB/UpdateGachaMetaDB.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf ./Packages/GachaKitHSR/Sources/GachaKitHSR/Assets/GachaMetaDB.json || true
-
-swift ./Script/HSRGachaMetaHashDBGenerator.swift > ./Packages/GachaKitHSR/Sources/GachaKitHSR/Assets/GachaMetaDB.json
-
-echo 'All tasks done.'
--- Script/ArchivedForFuturePurposes/GachaMetaDB/UpdateGachaMetaDB_Test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm -rf ./GachaMetaDB_GeneratedSpecimen_HSRv23.json || true
-
-swift ./HSRGachaMetaHashDBGenerator.swift > ./GachaMetaDB_GeneratedSpecimen_HSRv23.json
-
-echo 'All tasks done.'
