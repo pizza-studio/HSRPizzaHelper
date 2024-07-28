203770fcce250ad94aceba59cdaee94c4c5d27c9
ShikiSuen <shikisuen@outlook.com>
2024-07-28 22:03:12 +0800

    UpdateGachaMetaDBSPM // Linux compatibility.

--- Script/UpdateGachaMetaDBSPM.swift
+++ Script/UpdateGachaMetaDBSPM.swift
@@ -1,4 +1,48 @@
 import Foundation
+#if canImport(FoundationNetworking)
+import FoundationNetworking
+#endif
+
+// MARK: - URLSessionAsyncErrors
+
+/// Defines the possible errors
+public enum URLSessionAsyncErrors: Error {
+    case invalidUrlResponse, missingResponseData
+}
+
+/// An extension that provides async support for fetching a URL
+///
+/// Needed because the Linux version of Swift does not support async URLSession yet.
+extension URLSession {
+    /// A reimplementation of `URLSession.shared.data(from: url)` required for Linux
+    ///
+    /// - Parameter url: The URL for which to load data.
+    /// - Returns: Data and response.
+    ///
+    /// - Usage:
+    ///
+    ///     let (data, response) = try await URLSession.shared.asyncData(from: url)
+    public func asyncData(from url: URL) async throws -> (Data, URLResponse) {
+        try await withCheckedThrowingContinuation { continuation in
+            let task = self.dataTask(with: url) { data, response, error in
+                if let error = error {
+                    continuation.resume(throwing: error)
+                    return
+                }
+                guard let response = response as? HTTPURLResponse else {
+                    continuation.resume(throwing: URLSessionAsyncErrors.invalidUrlResponse)
+                    return
+                }
+                guard let data = data else {
+                    continuation.resume(throwing: URLSessionAsyncErrors.missingResponseData)
+                    return
+                }
+                continuation.resume(returning: (data, response))
+            }
+            task.resume()
+        }
+    }
+}
 
 // MARK: - GitTag
 
@@ -8,11 +52,7 @@ struct GitTag: Codable {
 
 func getLatestTag() async throws -> String? {
     let gitHubAPIURL = URL(string: "https://api.github.com/repos/pizza-studio/GachaMetaGenerator/tags")!
-
-    var request = URLRequest(url: gitHubAPIURL)
-    request.httpMethod = "GET"
-
-    let (data, _) = try await URLSession.shared.data(for: request)
+    let (data, _) = try await URLSession.shared.asyncData(from: gitHubAPIURL)
     let json = try JSONDecoder().decode([GitTag].self, from: data)
     return json.first?.name
 }
